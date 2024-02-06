1ff777395e500a291e178c944b5f827492f052fa
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-02-06 22:30:31 +0800

    alvistack/1.8.9
        git clean -xdf
        tar zcvf ../python-webob_1.8.9.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-webob.spec ../python-webob_1.8.9-1.spec
        cp ../python*-webob*1.8.9*.{gz,xz,spec,dsc} /osc/home\:alvistack/Pylons-webob-1.8.9/
        rm -rf ../python*-webob*1.8.9*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ docs_extras = [
 
 setup(
     name="WebOb",
-    version="2.0.0dev0",
+    version="1.8.9",
     description="WSGI request and response object",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
